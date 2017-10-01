55baae73c0a2df87c08061f59e4a5d66e2955213
Muazam Kamal <muazam.kamal@gmail.com>
2017-10-02 05:58:03 +0800

    - StudentDetail page fixed. - Added 1 more student in testdb

--- mit.py
+++ mit.py
@@ -10,6 +10,23 @@ class Management(tk.Tk):
         self.geometry("480x480")
         self.resizable(False,False)
 
+        #Variables of all the details for StudentDetail frame
+        global TitleOfStud
+        global NameOfStud
+        global ConOfStud
+        global EmConOfStud
+        global SubOfStud
+        global FeeOfStud
+        global OutFeeOfStud
+
+        TitleOfStud = tk.StringVar()
+        NameOfStud = tk.StringVar()
+        ConOfStud = tk.StringVar()
+        EmConOfStud = tk.StringVar()
+        SubOfStud = tk.StringVar()
+        FeeOfStud = tk.StringVar()
+        OutFeeOfStud = tk.StringVar()
+
         # Setting up frames or pages
         global container
         container = tk.Frame(self)
@@ -58,8 +75,8 @@ class MainMenu(tk.Frame):
         tutor_button = tk.Button(self, text="Student", command = lambda: controller.show_frame(Student), bg = "white")
         tutor_button.place(relx = 0.5, rely = 0.5, anchor = "center")
 
-        # quit_button = tk.Button(self, text="Quit", command = quit, bg = "white")
-        # quit_button.pack(side = "bottom")
+        quit_button = tk.Button(self, text="Quit", command = lambda: quit(), bg = "white")
+        quit_button.pack(side = "bottom", pady = 10)
 
 # Student frame
 class Student(tk.Frame):
@@ -95,13 +112,35 @@ class Student(tk.Frame):
             StudentName.append(StudentData[i][0])
             self.student_list_box.insert("end", StudentData[i][0])
 
-        self.student_list_box.pack()
+        self.student_list_box.pack(fill = "both")
+
+        # Assign a method for mouse click
+        self.student_list_box.bind('<<ListboxSelect>>', self.chooseNAME)
 
         menu_button = tk.Button(self, text="Main Menu", command = lambda: controller.show_frame(MainMenu), bg = "white")
         menu_button.pack(side = "bottom", padx = 10, pady = 10)
 
-        detail_button = tk.Button(self, text="Detail Page", command = lambda: controller.show_frame(StudentDetail), bg = "white")
-        detail_button.pack(side = "bottom", padx = 10, pady = 10)
+    def chooseNAME(self, event) :
+        # Get the selected/clicked name from the listbox and set it to the global variable TitleOfStud
+        selected = self.student_list_box.get(self.student_list_box.curselection()[0])
+        TitleOfStud.set(selected)
+
+        # Getting the index of the selected name
+        for index in range(0, len(StudentName)):
+            if StudentName[index] == selected:
+                break
+
+        # Individually set each of the details to the corresponding value from the StudentDate (with the help of index)
+        NameOfStud.set("Name: %s" % StudentData[index][0])
+        ConOfStud.set("Contact: %s" % StudentData[index][1])
+        EmConOfStud.set("Emergency Contact: %s" % StudentData[index][2])
+        SubOfStud.set("Subject: %s" % StudentData[index][3])
+        FeeOfStud.set("Fees: %s" % StudentData[index][4])
+        OutFeeOfStud.set("Outstanding Fees: %s" % StudentData[index][5])
+
+        # Display the StudentDetail frame
+        self.controller.show_frame(StudentDetail)
+
 
 class StudentDetail(tk.Frame):
     def __init__(self, parent, controller):
@@ -110,30 +149,19 @@ class StudentDetail(tk.Frame):
         self.configure(bg = "white")
         self.controller = controller
 
-        # Fetching selected value from listbox in "Student" frame
-        student_page = self.controller.get_page(Student)
-        NAMETOSEARCH = student_page.student_list_box.get("active")
-        print NAMETOSEARCH
-        # NAMETOSEARCH = "Shafiq"
-
-        DetailTitle = tk.Label(self, text= NAMETOSEARCH, font = 14, bg = "white")
+        DetailTitle = tk.Label(self, textvariable = TitleOfStud, font = 14, bg = "white")
         DetailTitle.pack(padx = 10, pady = 10)
 
-        #Checking index number
-        for index in range(0, len(StudentName)):
-            if StudentName[index] == NAMETOSEARCH:
-                break
-
-        Name = tk.Label(self, text="Name: " + StudentData[index][0], bg = "white")
-        Contact = tk.Label(self, text="Contact: " + StudentData[index][1], bg = "white")
-        EContact = tk.Label(self, text="Emergency Contact: " + StudentData[index][2], bg = "white")
-        Subject = tk.Label(self, text="Subject: " + StudentData[index][3], bg = "white")
-        TuitionFees = tk.Label(self, text="Tuition Fees: RM" + StudentData[index][4], bg = "white")
-        Outstanding = tk.Label(self, text="Outstanding Fees: RM" + StudentData[index][5], bg = "white")
+        Name = tk.Label(self, textvariable = NameOfStud, bg = "white")
+        Contact = tk.Label(self, textvariable = ConOfStud, bg = "white")
+        EContact = tk.Label(self, textvariable = EmConOfStud, bg = "white")
+        Subject = tk.Label(self, textvariable = SubOfStud, bg = "white")
+        TuitionFees = tk.Label(self, textvariable = FeeOfStud, bg = "white")
+        Outstanding = tk.Label(self, textvariable = OutFeeOfStud, bg = "white")
 
         Name.pack(side = "top")
-        EContact.pack(side = "top")
         Contact.pack(side = "top")
+        EContact.pack(side = "top")
         Subject.pack(side = "top")
         TuitionFees.pack(side = "top")
         Outstanding.pack(side = "top")
